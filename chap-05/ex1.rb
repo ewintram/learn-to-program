4f36ba53ae85ccd0f5aebb76b3d5b476707e4fda
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-05 10:31:25 +0000

    Chap05 1-2

---
+++ chap-05/ex1.rb
@@ -0,0 +1,8 @@
+# Full name greeting
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your second name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Hi " + first_name + " " + middle_name + " " + last_name + "."
---
+++ chap-05/ex2.rb
@@ -0,0 +1,5 @@
+# Bigger, better favorite number
+puts "What's your favourite number?"
+favourite_number = gets.chomp.to_i
+bigger_number = favourite_number + 1
+puts "That's cool but how about " + bigger_number.to_s + "?"
