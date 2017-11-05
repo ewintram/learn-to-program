02bf707cdc0dce32363e64ea138e74c64e1d6358
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-05 10:06:34 +0000

    Chap01 1

---
+++ chap-01/ex1.rb
@@ -0,0 +1,8 @@
+# Hours in a year
+puts 365 * 24
+# Minutes in a decade
+puts (10 * 365 + 2) * 24 * 60
+# Seconds old
+puts (28 * 365 + 7) * 24 * 60 * 60
+# Author's age
+puts 1160000000 / 60 / 60 / 24 / 365
