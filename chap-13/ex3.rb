026c37c7331f48b7da29f16fd874b74969673510
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-19 18:12:20 +0000

    Chap13 3

---
+++ chap-13/ex3.rb
@@ -0,0 +1,19 @@
+class Orange_tree
+
+  def initialize
+    @height = 1
+  end
+
+  def height
+    puts @height
+  end
+
+  def one_year_passes
+    @height = @height + 1
+    while @height > 4
+      @oranges = 1
+      
+    if @height == 8 exit
+  end
+
+end
